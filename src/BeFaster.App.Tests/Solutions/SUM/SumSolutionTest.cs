91c3cc4f702fb2c775d409eda792e30e3a2a3f36
sbx_user1051 <sbx_user1051@169.254.141.29>
2020-02-09 10:50:49 +0000

    Sun Feb 09 10:50:49 UTC 2020

--- src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
@@ -6,8 +6,12 @@ namespace BeFaster.App.Tests.Solutions.SUM
     [TestFixture]
     public class SumSolutionTest
     {
-        [TestCase()]
-        public int InvalidInput(int x, int y)
+        [TestCase(-4, -5)]
+        [TestCase(1, -5)]
+        [TestCase(-1, 5)]
+        [TestCase(0, -7)]
+        [TestCase(-7, 0)]
+        public void ShouldReturnMinus1_When_InvalidInput(int x, int y)
         {
             // When
             var result = SumSolution.Sum(x, y);
@@ -18,10 +22,11 @@ namespace BeFaster.App.Tests.Solutions.SUM
 
 
         [TestCase(1, 1, ExpectedResult = 2)]
-        public int ComputeSum(int x, int y)
+        public int ComputeSum_WhenInputIsValid(int x, int y)
         {
             return SumSolution.Sum(x, y);
         }
     }
 }
+
 
