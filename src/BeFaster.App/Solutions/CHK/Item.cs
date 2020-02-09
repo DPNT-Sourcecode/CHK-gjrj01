7281daf0ae76df1f0765351d9aa2a49f33276898
sbx_user1051 <sbx_user1051@169.254.141.29>
2020-02-09 12:59:22 +0000

    Sun Feb 09 12:59:22 UTC 2020

---
+++ src/BeFaster.App/Solutions/CHK/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK
+{
+    public class Item
+    {
+        public string Sku { get; set; }
+
+        public int Price { get; set; }
+
+        public string Offer { get; set; }
+    }
+}
