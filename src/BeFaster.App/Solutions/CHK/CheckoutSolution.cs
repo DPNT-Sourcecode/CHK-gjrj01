c23ca878f5a7527468c005e2f628f0e5b8a74156
sbx_user1051 <sbx_user1051@169.254.141.29>
2020-02-09 11:20:33 +0000

    Sun Feb 09 11:20:33 UTC 2020

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,4 +1,8 @@
-﻿using BeFaster.Runner.Exceptions;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using BeFaster.Runner.Exceptions;
 
 namespace BeFaster.App.Solutions.CHK
 {
@@ -6,15 +10,31 @@ namespace BeFaster.App.Solutions.CHK
     {
         public static int ComputePrice(string skus)
         {
-            var items = GetListFromBasket()
+            var items = GetListFromBasket(skus);
+            int total = 0;
 
-            switch (switch_on)
+            foreach (var item in items)
             {
-                default:
+                switch (item)
+                {
+                    case "A":
+                        total += 50;
+                        break;
+                    default:
+                        break;
+                }
             }
-
             throw new SolutionNotImplementedException();
+        }
+
+        private static IList<string> GetListFromBasket(string skus)
+        {
+            // there are no requirements about how the list of skus is provided.
+            // I would ask for the PO about the format of the string SKUs
+            // i.e. what separator? is it case sensitive a so on...
+            // I'm assuming the PO replied saying the string is case sensitive and separator is , (comma)
+
+            return skus.Split(',').Select(x => skus.Trim().ToUpper()s).ToList();
         }
     }
 }
-
