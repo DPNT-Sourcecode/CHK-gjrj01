4c34588fdfe56ee7b4bf17607636848bfc1e9fdb
sbx_user1051 <sbx_user1051@169.254.141.29>
2020-02-09 12:56:22 +0000

    Sun Feb 09 12:56:22 UTC 2020

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -14,94 +14,18 @@ namespace BeFaster.App.Solutions.CHK
 
             // 2E get 1 B free
             items.ApplyPromotion("E", 2, "B", 1);
-
             items.ApplyPromotion("N", 3, "M", 1);
             items.ApplyPromotion("R", 3, "Q", 1);
 
             int total = 0;
 
-            foreach (var item in items)
-            {
-                switch (item.Key)
-                {
-                    case "A":
-                        total += PriceWithOffer(item.Value, 50, new Dictionary<int, int> { { 5, 200 }, { 3, 130 } });
-                        break;
-
-                    case "B":
-                        total += PriceWithOffer(item.Value, 30, new Dictionary<int, int> { { 2, 45 } });
-                        break;
-
-                    case "C":
-                        total += item.Value * 20;
-                        break;
-
-                    case "D":
-                        total += item.Value * 15;
-                        break;
-
-                    case "E":
-                        total += item.Value * 40;
-                        break;
-
-                    case "F":
-                        total += PriceWithOffer(item.Value, 10, new Dictionary<int, int> { { 3, 20 } });
-                        break;
-
-                    case "G":
-                        total += item.Value * 20;
-                        break;
-
-                    case "H":
-                        total += PriceWithOffer(item.Value, 10, new Dictionary<int, int> { { 5, 45 }, { 10, 80 } });
-                        break;
-
-                    case "I":
-                        total += item.Value * 35;
-                        break;
-
-                    case "J":
-                        total += item.Value * 60;
-                        break;
-
-                    case "K":
-                        total += PriceWithOffer(item.Value, 80, new Dictionary<int, int> { { 2, 150 } });
-                        break;
-
-                    case "L":
-                        total += item.Value * 90;
-                        break;
-
-                    case "M":
-                        total += item.Value * 15;
-                        break;
-
-                    case "N":
-                        total += item.Value * 40;
-                        break;
-
-                    case "O":
-                        total += item.Value * 10;
-                        break;
-
-                    case "P":
-                        total += PriceWithOffer(item.Value, 50, new Dictionary<int, int> { { 5, 200 } });
-                        break;
-
-                    case "Q":
-                        total += PriceWithOffer(item.Value, 30, new Dictionary<int, int> { { 3, 80 } });
-                        break;
-
-                    case "R":
-                        total += item.Value * 50;
-                        break;
-
-                    default:
-                        return -1;
-                }
-            }
 
             return total;
+        }
+
+        private void LoadPrices()
+        {
+
         }
 
         private static int PriceWithOffer(int qty, int pricePerSingle, Dictionary<int, int> qtyPriceDiscount)
